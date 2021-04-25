db7a09b0a1668c8cead70833f40328e96c452551
建 王 <1472318683@qq.com>
2021-04-25 18:17:59 +0800

    11

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+class Dog
+{
+public:
+	void setDog(string new_name = "default", int new_age = 1, int new_weight = 1);
+	void showDog();
+private:
+	int age, weight;
+	string name;
+};
+void Dog::setDog(string new_name, int new_age, int new_weight)
+{
+	name = new_name;
+	age = new_age;
+	weight = new_weight;
+};
+void Dog::showDog()
+{
+	cout << "这只狗的名字为：" << name << endl;
+	cout << "这只狗的年龄为：" << age << "岁" << endl;
+	cout << "这只狗的体重为：" << weight << "千克" << endl;
+};
+
+int main()
+{
+	Dog mydog1;
+	mydog1.setDog("Tom", 7, 35);
+	mydog1.showDog();
+	Dog mydog2;
+	mydog2.setDog("Jack", 5, 20);
+	mydog2.showDog();
+}
