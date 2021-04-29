95c4c1966771ba2d57096ef7f455a46954c5237d
Deng13148 <63764335+Deng13148@users.noreply.github.com>
2021-04-29 12:09:50 +0800

    Update 5-13.cpp

--- Project1/Project1/5-13.cpp
+++ Project1/Project1/5-13.cpp
@@ -4,31 +4,55 @@ using namespace std;
 class Y;
 class X {
 public:
-	friend int h(X*);
-	friend int Y::g(X*);
+	friend class Y;
+	friend int h();
+	//friend int Y::g();
 	friend class Z;
-	void setI(int newi);
+	void setI();
 private:
 	static int i;
 };
 class Y {
 public:
-	int g(X*);
+	int g();
 private:
 
 };
 class Z {
 public:
-	int f(X*);
+	int f();
 private:
 
 };
-void X::setI(int newi) {
+int X::i = 0;
+void X::setI() {
+	int newi;
+	cout << "请输入i的值：";
+	cin >> newi;
 	i = newi;
 }
-int Y::g(X*) {
-	
+int Y::g() {
+	X::i = (X::i)+1;
+	cout << "调用g（）后，i：" << X::i<<endl;
+	return X::i;
+}
+int Z::f() {
+	X::i = (X::i)+5;
+	cout << "调用f（）后，i：" << X::i<<endl;
+	return X::i;
+}
+int h() {
+	X::i = (X::i)+10;
+	cout << "调用h（）后，i：" << X::i<<endl;
+	return X::i;
 }
 int main() {
-
+	X myx;
+	Y myy;
+	Z myz;
+	myx.setI();
+	myy.g();
+	myz.f();
+	h();
+	return 0;
 }
